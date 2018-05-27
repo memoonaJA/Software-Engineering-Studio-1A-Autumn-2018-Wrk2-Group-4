7ff74a3fa56dd49005e8cfccffd086c25ecc9502
Daniel Jacob <daniel.jacob@student.uts.edu.au>
2018-05-27 17:27:23 +1000

    leaderboard fucntion v1.0 implemented

---
+++ app/src/main/java/com/example/softwaregroup4/group4_fitnessapplication/leaderboard.java
@@ -0,0 +1,13 @@
+package com.example.softwaregroup4.group4_fitnessapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class leaderboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_leaderboard);
+    }
+}
