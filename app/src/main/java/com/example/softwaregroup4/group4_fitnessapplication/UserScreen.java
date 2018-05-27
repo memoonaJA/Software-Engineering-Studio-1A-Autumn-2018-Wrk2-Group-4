ddd09cb069666b116e6650e9af49be4b78dfa51e
lukekoko <lukekoko1@gmail.com>
2018-05-27 20:39:48 +1000

    - Added leaderboards - feature by Daniel

--- app/src/main/java/com/example/softwaregroup4/group4_fitnessapplication/UserScreen.java
+++ app/src/main/java/com/example/softwaregroup4/group4_fitnessapplication/UserScreen.java
@@ -68,6 +68,11 @@ public class UserScreen extends AppCompatActivity {
         startActivity(intent);
     }
 
+    public void changeleader(View view) {
+        Intent intent = new Intent(this, leaderboard.class);
+        startActivity(intent);
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
